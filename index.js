066c747a66723f0d56fbb804e6a3bc8ad9129891
k3tanyadav <ketany798@gmail.com>
2023-05-24 01:14:28 +0530

    create first route and handle it

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const port = 8000;
 
 const app = express();
 
+//all requests will be handled by routes
+app.use('/', require('./routes'));
+
 app.listen(port, function(err){
     if(err){
         console.log('could not run the server');
