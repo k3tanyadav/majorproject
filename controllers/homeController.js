a1ae818797da30bf3bf6c0d8333b370b8c232c0a
k3tanyadav <ketany798@gmail.com>
2023-05-24 01:43:00 +0530

    set up the view engine and render home.ejs

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,5 @@
 module.exports.home = function(req,res){
-    res.send('<h1>welcome to home page!!</h1>');
+    return res.render('home', {
+        title: 'HOME PAGE :)'
+    })
 }
--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 //all requests will be handled by routes
 app.use('/', require('./routes'));
 
+// use ejs views engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log('could not run the server');
