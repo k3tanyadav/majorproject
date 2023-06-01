fbd8dbc2aca8f2f013d4863da21481c768f55dc6
k3tanyadav <ketany798@gmail.com>
2023-06-02 02:58:53 +0530

    handle delete post,and subsequently delete all comments of that post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,4 +1,5 @@
 const Post = require('../models/posts');
+const Comment = require('../models/comments');
 
 module.exports.create = function(req,res){
     Post.create({
@@ -6,5 +7,21 @@ module.exports.create = function(req,res){
         user : req.user._id,
     }).then(()=>{
         res.redirect('back');
+    })
+}
+
+//delete post
+module.exports.destroy = function(req,res){
+    Post.findById(req.params.id).then((post)=>{
+        if(post.user == req.user.id){
+            Post.deleteOne({_id : post._id}).then(()=>{
+                Comment.deleteMany({post : req.params.id}).then(()=>{
+                    return res.redirect('back');
+                });
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
     })
 }
