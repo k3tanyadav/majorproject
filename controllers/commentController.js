db253eab406757a0ba005f91f65871abb9bec0b8
k3tanyadav <ketany798@gmail.com>
2023-06-06 00:49:48 +0530

    change callback chains to async/await

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,33 +1,31 @@
 const Comment = require('../models/comments');
 const Post = require('../models/posts');
 
-module.exports.create = function(req,res){
-    Post.findById(req.body.post).then((post)=>{
-        Comment.create({
-            content : req.body.content,
-            user : req.user._id,
-            post : req.body.post
-        }).then((comment)=>{
-            post.comments.push(comment);
-            post.save();
-            return res.redirect('/');
-        })
+module.exports.create = async function(req,res){
+    let post = await Post.findById(req.body.post);
+
+    let comment = await Comment.create({
+        content : req.body.content,
+        user : req.user._id,
+        post : req.body.post
     })
+
+    post.comments.push(comment);
+    post.save();
+    return res.redirect('/');
 }
 
 //delete comment
-module.exports.destroy = function(req,res){
-    Comment.findById(req.params.id).then((comment)=>{
-        if(req.user.id == comment.user){
-            let postId = comment.post;
-            Comment.deleteOne({_id : req.params.id}).then(()=>{
-                Post.findByIdAndUpdate(postId, { $pull : { comments : req.params.id } }).then(()=>{
-                    return res.redirect('back');
-                });
-            });
-        }
-        else{
-            return res.redirect('back');
-        }
-    });
+module.exports.destroy = async function(req,res){
+    let comment = await Comment.findById(req.params.id);
+
+    if(req.user.id == comment.user){
+        let postId = comment.post;
+        await Comment.deleteOne({_id : req.params.id});
+        await Post.findByIdAndUpdate(postId, { $pull : { comments : req.params.id } });
+        return res.redirect('back');
+    }
+    else{
+        return res.redirect('back');
+    }
 }
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,17 +1,14 @@
 const Post = require('../models/posts');
 const User = require('../models/users');
 
-module.exports.home = function(req,res){
-    Post.find({})
-    .populate('user')
-    .populate({path : 'comments', populate : {path : 'user'}})
-    .then((data)=>{
-        User.find({}).then((allUsers)=>{
-            return res.render('home', {
-                title : "HOME",
-                posts : data,
-                allUsers : allUsers
-            })
-        })
+module.exports.home = async function(req,res){
+    let posts = await Post.find({}).populate('user').populate({path : 'comments', populate : {path : 'user'}})
+
+    let allUsers = await User.find({});
+
+    return res.render('home', {
+        title : "HOME",
+        posts : posts,
+        allUsers : allUsers
     })
 }
