123a801c60d3db2ce3481cda9a0acabc0dec83ed
k3tanyadav <ketany798@gmail.com>
2023-06-15 04:48:06 +0530

    sign in a user and generate their token

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const postsAPI = require('../../../controllers/api/v1/posts_api');
 
-router.get('/', postsAPI.index);
+router.use('/posts', require('./posts'));
+router.use('/users', require('./users'));
 
 module.exports = router;
